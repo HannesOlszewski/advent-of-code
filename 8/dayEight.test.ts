3974a80f47af0f11b860feb5ff9a6ddb8c7611ab
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-12-23 17:06:24 +0100

    feat: day eight, part two

--- 8/dayEight.test.ts
+++ 8/dayEight.test.ts
@@ -1,4 +1,4 @@
-import { partOne } from ".";
+import { partOne, partTwo } from ".";
 
 const input1 = `RL
 
@@ -16,6 +16,17 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`;
 
+const input3 = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
+
 describe("dayEight", () => {
   describe("partOne", () => {
     it("returns 2", () => {
@@ -24,6 +35,12 @@ describe("dayEight", () => {
 
     it("returns 6", () => {
       expect(partOne(input2)).toEqual(6);
+    });
+  });
+
+  describe("partTwo", () => {
+    it("returns 6", () => {
+      expect(partTwo(input3)).toEqual(6);
     });
   });
 });
--- 8/index.ts
+++ 8/index.ts
@@ -47,12 +47,99 @@ function countStepsTillEnd(
   return steps;
 }
 
+function isPrime(number: number): boolean {
+  if (number <= 1) {
+    return false;
+  }
+
+  if (number <= 3) {
+    return true;
+  }
+
+  if (number % 2 === 0 || number % 3 === 0) {
+    return false;
+  }
+
+  for (let i = 5; i * i <= number; i = i + 6) {
+    if (number % i === 0 || number % (i + 2) === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function calculateLcm(numbers: number[]): number {
+  let divisor: number = 0;
+  let i = 1;
+
+  while (divisor === 0) {
+    if (isPrime(i)) {
+      for (const number of numbers) {
+        if (number % i === 0) {
+          divisor = number / i;
+        }
+      }
+    }
+
+    i++;
+  }
+
+  const primes = numbers.map((number) => number / divisor);
+
+  return primes.reduce((acc, prime) => acc * prime, divisor);
+}
+
+function countStepsTillEndParallel(
+  start: string[],
+  isEnd: (node: string) => boolean,
+  nodes: Record<string, Node>,
+  instructions: Instruction[]
+): number {
+  let currentNodes = start;
+  let steps = 0;
+  let firstEndReached: { node: string; endAtStep: number }[] = start.map(
+    (node) => ({
+      node,
+      endAtStep: 0,
+    })
+  );
+
+  while (firstEndReached.some((end) => end.endAtStep === 0)) {
+    const nextNodes = currentNodes.map((node) => {
+      return nodes[node][instructions[steps % instructions.length]];
+    });
+
+    nextNodes.forEach((node, index) => {
+      if (isEnd(node) && firstEndReached[index].endAtStep === 0) {
+        firstEndReached[index].endAtStep = steps + 1;
+      }
+    });
+
+    currentNodes = nextNodes;
+    steps++;
+  }
+
+  const firstEndReachedSteps = firstEndReached.map((end) => end.endAtStep);
+
+  return calculateLcm(firstEndReachedSteps);
+}
+
 export function partOne(input: string): number {
   const { instructions, nodes } = parseInput(input);
 
   return countStepsTillEnd("AAA", "ZZZ", nodes, instructions);
 }
 
+export function partTwo(input: string): number {
+  const { instructions, nodes } = parseInput(input);
+
+  const startNodes = Object.keys(nodes).filter((node) => node.endsWith("A"));
+  const isEndNode = (node: string) => node.endsWith("Z");
+
+  return countStepsTillEndParallel(startNodes, isEndNode, nodes, instructions);
+}
+
 export function dayEight(input: string): [number, number] {
-  return [partOne(input), 0];
+  return [partOne(input), partTwo(input)];
 }
