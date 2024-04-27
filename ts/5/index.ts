2319f09e2030147ab002ae610bede05446f924c9
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2024-04-27 17:08:22 +0200

    wip(ts): implement day five in typescript
    Part one works, part two doesn't error but calculates the wrong number.

--- ts/5/index.ts
+++ ts/5/index.ts
@@ -1,131 +1,295 @@
-import fs from "fs";
-
-interface Range {
-  rangeStart: number;
-  rangeLength: number;
-}
-
 interface Mapping {
-  targetStart: number;
-  sourceStart: number;
-  periodLength: number;
+  firstTarget: number;
+  lastTarget: number;
+  firstSource: number;
+  lastSource: number;
 }
 
-function parseSeeds(line: string): Range[] {
-  if (!line.startsWith("seeds:")) {
-    throw new Error("Invalid seeds line");
-  }
+function parseMapping(mappingLines: Array<string>): Array<Mapping> {
+  return mappingLines
+    .map((line) => {
+      const parts = line.split(" ");
+      const firstTarget = Number.parseInt(parts[0]);
+      const firstSource = Number.parseInt(parts[1]);
+      const ranges = Number.parseInt(parts[2]);
 
-  const values = line
-    .split(" ")
-    // Remove "seeds:"
-    .slice(1);
-
-  const ranges: Range[] = [];
-
-  for (let i = 0; i < values.length; i += 2) {
-    const rangeStart = parseInt(values[i]);
-    const rangeLength = parseInt(values[i + 1]);
-
-    ranges.push({ rangeStart, rangeLength });
-  }
-
-  return ranges;
+      return {
+        firstTarget,
+        lastTarget: firstTarget + ranges - 1,
+        firstSource,
+        lastSource: firstSource + ranges - 1,
+      };
+    })
+    .sort((a, b) => a.firstTarget - b.firstTarget);
 }
 
-function parseMaps(lines: string[]): Mapping[] {
-  const mappings: Mapping[] = [];
+function findTarget(source: number, mappings: Array<Mapping>): number {
+  for (const mapping of mappings) {
+    if (source >= mapping.firstSource && source <= mapping.lastSource) {
+      return mapping.firstTarget + (source - mapping.firstSource);
+    }
+  }
 
-  for (const line of lines) {
-    if (line.length === 0) {
-      continue;
+  return source;
+}
+
+function findSourceMappings(
+  firstTarget: number,
+  lastTarget: number,
+  mappings: Array<Mapping>,
+): Array<Mapping> {
+  const sourceMappings: Array<Mapping> = [];
+
+  for (const mapping of mappings) {
+    if (
+      mapping.firstTarget <= firstTarget &&
+      mapping.lastTarget >= lastTarget
+    ) {
+      sourceMappings.push({
+        firstTarget,
+        lastTarget,
+        firstSource: mapping.firstSource + (firstTarget - mapping.firstTarget),
+        lastSource: mapping.firstSource + (lastTarget - mapping.firstTarget),
+      });
     }
 
-    const [targetStart, sourceStart, periodLength] = line.split(" ");
+    if (
+      mapping.firstTarget >= firstTarget &&
+      mapping.lastTarget <= lastTarget
+    ) {
+      sourceMappings.push(mapping);
+    }
 
-    mappings.push({
-      targetStart: parseInt(targetStart),
-      sourceStart: parseInt(sourceStart),
-      periodLength: parseInt(periodLength),
+    if (
+      mapping.firstTarget <= firstTarget &&
+      mapping.lastTarget <= lastTarget &&
+      mapping.lastTarget >= firstTarget
+    ) {
+      sourceMappings.push({
+        firstTarget,
+        lastTarget: mapping.lastTarget,
+        firstSource: mapping.firstSource + (firstTarget - mapping.firstTarget),
+        lastSource: mapping.lastSource,
+      });
+    }
+
+    if (
+      mapping.firstTarget >= firstTarget &&
+      mapping.lastTarget >= lastTarget &&
+      mapping.firstTarget <= lastTarget
+    ) {
+      sourceMappings.push({
+        firstTarget: mapping.firstTarget,
+        lastTarget,
+        firstSource: mapping.firstSource,
+        lastSource: mapping.firstSource + (lastTarget - mapping.firstTarget),
+      });
+    }
+  }
+
+  if (sourceMappings.length === 0) {
+    sourceMappings.push({
+      firstTarget,
+      lastTarget,
+      firstSource: firstTarget,
+      lastSource: lastTarget,
     });
   }
 
-  return mappings;
+  return sourceMappings;
 }
-
-const sections = fs.readFileSync("input.txt", "utf8").split("\n\n");
-
-const seeds = parseSeeds(sections[0]).sort(
-  (a, b) => a.rangeStart - b.rangeStart
-);
-const mappings = sections
-  .slice(1)
-  .map((section) => parseMaps(section.split("\n").slice(1)))
-  .sort((a, b) => a[0].targetStart - b[0].targetStart);
-
-const currentTargets: Range[] = seeds;
-
-for (const mapping of mappings) {
-  const currentTargetsCopy = [...currentTargets];
-  currentTargets.splice(0, currentTargets.length);
-
-  for (const target of currentTargetsCopy) {
-    const targetEnd = target.rangeStart + target.rangeLength;
-
-    for (const map of mapping) {
-      const sourceEnd = map.sourceStart + map.periodLength;
-
-      if (target.rangeStart > sourceEnd || map.sourceStart > targetEnd) {
-        continue;
-      }
-
-      // In case of partial match add the non-overlapped parts back to the list
-      if (target.rangeStart < map.sourceStart) {
-        currentTargets.push({
-          rangeStart: target.rangeStart,
-          rangeLength: map.sourceStart - target.rangeStart,
-        });
-      }
-
-      if (targetEnd > sourceEnd) {
-        currentTargets.push({
-          rangeStart: sourceEnd,
-          rangeLength: targetEnd - sourceEnd,
-        });
-      }
-
-      // Add the overlapped part to the list
-      const [offset, overlapStart] =
-        map.sourceStart < target.rangeStart
-          ? [target.rangeStart - map.sourceStart, target.rangeStart]
-          : [0, map.sourceStart];
-      const overlapRangeLength = Math.min(targetEnd, sourceEnd) - overlapStart;
-
-      currentTargets.push({
-        rangeStart: map.targetStart + offset,
-        rangeLength: overlapRangeLength,
-      });
-      break;
-    }
-
-    // If no mapping found add the whole target to the list
-    if (currentTargets.length === 0) {
-      console.log("No mapping found for", target);
-      currentTargets.push(target);
-    }
-  }
-}
-
-const lowest = Math.min(...currentTargets.map((t) => t.rangeStart));
-
-console.log(lowest);
 
 export function partOne(input: string): number {
-  return 0;
+  const inputParts = input.split("\n\n");
+  const seeds = inputParts[0].split(" ").slice(1).map(Number);
+  const seedToSoilMap = parseMapping(inputParts[1].split("\n").slice(1));
+  const soilToFertilizerMap = parseMapping(inputParts[2].split("\n").slice(1));
+  const fertilizerToWaterMap = parseMapping(inputParts[3].split("\n").slice(1));
+  const waterToLightMap = parseMapping(inputParts[4].split("\n").slice(1));
+  const lightToTemperatureMap = parseMapping(
+    inputParts[5].split("\n").slice(1),
+  );
+  const temperatureToHumidityMap = parseMapping(
+    inputParts[6].split("\n").slice(1),
+  );
+  const humidityToLocationMap = parseMapping(
+    inputParts[7].split("\n").slice(1),
+  );
+
+  let minLocation = -1;
+
+  for (const seed of seeds) {
+    const soil = findTarget(seed, seedToSoilMap);
+    const fertilizer = findTarget(soil, soilToFertilizerMap);
+    const water = findTarget(fertilizer, fertilizerToWaterMap);
+    const light = findTarget(water, waterToLightMap);
+    const temperature = findTarget(light, lightToTemperatureMap);
+    const humidity = findTarget(temperature, temperatureToHumidityMap);
+    const location = findTarget(humidity, humidityToLocationMap);
+
+    if (minLocation === -1 || location < minLocation) {
+      minLocation = location;
+    }
+  }
+
+  return minLocation;
 }
 
 export function partTwo(input: string): number {
-  return 0;
+  const inputParts = input.split("\n\n");
+  const seedsParts = inputParts[0].split(" ").slice(1);
+  const seeds: Array<Mapping> = [];
+  for (let i = 0; i < seedsParts.length; i += 2) {
+    const first = Number.parseInt(seedsParts[i]);
+    const range = Number.parseInt(seedsParts[i + 1]);
+
+    seeds.push({
+      firstSource: first,
+      lastSource: first + range - 1,
+      firstTarget: first,
+      lastTarget: first + range - 1,
+    });
+  }
+  const seedToSoilMap = parseMapping(inputParts[1].split("\n").slice(1));
+  const soilToFertilizerMap = parseMapping(inputParts[2].split("\n").slice(1));
+  const fertilizerToWaterMap = parseMapping(inputParts[3].split("\n").slice(1));
+  const waterToLightMap = parseMapping(inputParts[4].split("\n").slice(1));
+  const lightToTemperatureMap = parseMapping(
+    inputParts[5].split("\n").slice(1),
+  );
+  const temperatureToHumidityMap = parseMapping(
+    inputParts[6].split("\n").slice(1),
+  );
+  const humidityToLocationMap = parseMapping(
+    inputParts[7].split("\n").slice(1),
+  );
+
+  if (humidityToLocationMap[0].firstTarget > 0) {
+    humidityToLocationMap.unshift({
+      firstSource: 0,
+      lastSource: humidityToLocationMap[0].firstSource - 1,
+      firstTarget: 0,
+      lastTarget: humidityToLocationMap[0].firstSource - 1,
+    });
+  }
+
+  for (const locationMapping of humidityToLocationMap) {
+    const humidityMappings = findSourceMappings(
+      locationMapping.firstSource,
+      locationMapping.lastSource,
+      humidityToLocationMap,
+    );
+
+    for (const humidityMapping of humidityMappings) {
+      const temperatureMappings = findSourceMappings(
+        humidityMapping.firstSource,
+        humidityMapping.lastSource,
+        temperatureToHumidityMap,
+      );
+
+      for (const temperatureMapping of temperatureMappings) {
+        const lightMappings = findSourceMappings(
+          temperatureMapping.firstSource,
+          temperatureMapping.lastSource,
+          lightToTemperatureMap,
+        );
+
+        for (const lightMapping of lightMappings) {
+          const waterMappings = findSourceMappings(
+            lightMapping.firstSource,
+            lightMapping.lastSource,
+            waterToLightMap,
+          );
+
+          for (const waterMapping of waterMappings) {
+            const fertilizerMappings = findSourceMappings(
+              waterMapping.firstSource,
+              waterMapping.lastSource,
+              fertilizerToWaterMap,
+            );
+
+            for (const fertilizerMapping of fertilizerMappings) {
+              const soilMappings = findSourceMappings(
+                fertilizerMapping.firstSource,
+                fertilizerMapping.lastSource,
+                soilToFertilizerMap,
+              );
+
+              for (const soilMapping of soilMappings) {
+                const seedMappings = findSourceMappings(
+                  soilMapping.firstSource,
+                  soilMapping.lastSource,
+                  seedToSoilMap,
+                );
+
+                for (const seedMapping of seedMappings) {
+                  for (const seed of seeds) {
+                    let firstSeed = -1;
+
+                    if (
+                      seed.firstSource <= seedMapping.firstSource &&
+                      seed.lastSource >= seedMapping.lastSource
+                    ) {
+                      firstSeed = seedMapping.firstSource;
+                    }
+
+                    if (
+                      seed.firstSource >= seedMapping.firstSource &&
+                      seed.lastSource <= seedMapping.lastSource
+                    ) {
+                      firstSeed = seed.firstSource;
+                    }
+
+                    if (
+                      seed.firstSource <= seedMapping.firstSource &&
+                      seed.lastSource <= seedMapping.lastSource &&
+                      seed.lastSource >= seedMapping.firstSource
+                    ) {
+                      firstSeed = seedMapping.firstSource;
+                    }
+
+                    if (
+                      seed.firstSource >= seedMapping.firstSource &&
+                      seed.lastSource >= seedMapping.lastSource &&
+                      seed.firstSource <= seedMapping.lastSource
+                    ) {
+                      firstSeed = seed.firstSource;
+                    }
+
+                    if (firstSeed !== -1) {
+                      const soil = findTarget(firstSeed, seedToSoilMap);
+                      const fertilizer = findTarget(soil, soilToFertilizerMap);
+                      const water = findTarget(
+                        fertilizer,
+                        fertilizerToWaterMap,
+                      );
+                      const light = findTarget(water, waterToLightMap);
+                      const temperature = findTarget(
+                        light,
+                        lightToTemperatureMap,
+                      );
+                      const humidity = findTarget(
+                        temperature,
+                        temperatureToHumidityMap,
+                      );
+                      const location = findTarget(
+                        humidity,
+                        humidityToLocationMap,
+                      );
+
+                      return location;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return -1;
 }
 
 export function dayFive(input: string): [number, number] {
--- ts/main.ts
+++ ts/main.ts
@@ -3,7 +3,7 @@ import { dayOne } from "./1";
 import { dayTwo } from "./2";
 import { dayThree } from "./3";
 import { dayFour } from "./4";
-// import { dayFive } from "./5";
+import { dayFive } from "./5";
 import { daySix } from "./6";
 import { daySeven } from "./7";
 import { dayEight } from "./8";
@@ -14,138 +14,138 @@ import { dayTwelve } from "./12";
 import { dayThirteen } from "./13";
 
 const numberStrings = {
-	1: "one",
-	2: "two",
-	3: "three",
-	4: "four",
-	5: "five",
-	6: "six",
-	7: "seven",
-	8: "eight",
-	9: "nine",
-	10: "ten",
-	11: "eleven",
-	12: "twelve",
-	13: "thirteen",
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  11: "eleven",
+  12: "twelve",
+  13: "thirteen",
 };
 
 interface DayResult {
-	day: number;
-	partOne: number;
-	partTwo: number;
-	time: number;
+  day: number;
+  partOne: number;
+  partTwo: number;
+  time: number;
 }
 
 function printAsTable(results: DayResult[]) {
-	const dayHeaderLabel = "Day";
-	const partOneHeaderLabel = "Part One";
-	const partTwoHeaderLabel = "Part Two";
-	const timeHeaderLabel = "Time (ms)";
+  const dayHeaderLabel = "Day";
+  const partOneHeaderLabel = "Part One";
+  const partTwoHeaderLabel = "Part Two";
+  const timeHeaderLabel = "Time (ms)";
 
-	let maxDayCharLength = dayHeaderLabel.length;
-	let maxPartOneCharLength = partOneHeaderLabel.length;
-	let maxPartTwoCharLength = partTwoHeaderLabel.length;
-	let maxTimeCharLength = timeHeaderLabel.length;
+  let maxDayCharLength = dayHeaderLabel.length;
+  let maxPartOneCharLength = partOneHeaderLabel.length;
+  let maxPartTwoCharLength = partTwoHeaderLabel.length;
+  let maxTimeCharLength = timeHeaderLabel.length;
 
-	for (const result of results) {
-		maxDayCharLength = Math.max(maxDayCharLength, result.day.toString().length);
-		maxPartOneCharLength = Math.max(
-			maxPartOneCharLength,
-			result.partOne.toString().length,
-		);
-		maxPartTwoCharLength = Math.max(
-			maxPartTwoCharLength,
-			result.partTwo.toString().length,
-		);
-		maxTimeCharLength = Math.max(
-			maxTimeCharLength,
-			result.time.toString().length,
-		);
-	}
+  for (const result of results) {
+    maxDayCharLength = Math.max(maxDayCharLength, result.day.toString().length);
+    maxPartOneCharLength = Math.max(
+      maxPartOneCharLength,
+      result.partOne.toString().length,
+    );
+    maxPartTwoCharLength = Math.max(
+      maxPartTwoCharLength,
+      result.partTwo.toString().length,
+    );
+    maxTimeCharLength = Math.max(
+      maxTimeCharLength,
+      result.time.toString().length,
+    );
+  }
 
-	const header = [
-		dayHeaderLabel.padEnd(maxDayCharLength),
-		partOneHeaderLabel.padEnd(maxPartOneCharLength),
-		partTwoHeaderLabel.padEnd(maxPartTwoCharLength),
-		timeHeaderLabel.padEnd(maxTimeCharLength),
-	].join(" | ");
+  const header = [
+    dayHeaderLabel.padEnd(maxDayCharLength),
+    partOneHeaderLabel.padEnd(maxPartOneCharLength),
+    partTwoHeaderLabel.padEnd(maxPartTwoCharLength),
+    timeHeaderLabel.padEnd(maxTimeCharLength),
+  ].join(" | ");
 
-	const separator = "-".repeat(header.length);
+  const separator = "-".repeat(header.length);
 
-	console.log(header);
-	console.log(separator);
+  console.log(header);
+  console.log(separator);
 
-	for (const result of results) {
-		const row = [
-			result.day.toString().padEnd(maxDayCharLength),
-			result.partOne.toString().padEnd(maxPartOneCharLength),
-			result.partTwo.toString().padEnd(maxPartTwoCharLength),
-			result.time.toString().padEnd(maxTimeCharLength),
-		].join(" | ");
+  for (const result of results) {
+    const row = [
+      result.day.toString().padEnd(maxDayCharLength),
+      result.partOne.toString().padEnd(maxPartOneCharLength),
+      result.partTwo.toString().padEnd(maxPartTwoCharLength),
+      result.time.toString().padEnd(maxTimeCharLength),
+    ].join(" | ");
 
-		console.log(row);
-	}
+    console.log(row);
+  }
 
-	console.log(separator);
+  console.log(separator);
 }
 
 function main() {
-	const availableDays = {
-		1: dayOne,
-		2: dayTwo,
-		3: dayThree,
-		4: dayFour,
-		// 5: dayFive,
-		6: daySix,
-		7: daySeven,
-		8: dayEight,
-		9: dayNine,
-		10: dayTen,
-		11: dayEleven,
-		12: dayTwelve,
-		13: dayThirteen,
-	};
+  const availableDays = {
+    1: dayOne,
+    2: dayTwo,
+    3: dayThree,
+    4: dayFour,
+    5: dayFive,
+    6: daySix,
+    7: daySeven,
+    8: dayEight,
+    9: dayNine,
+    10: dayTen,
+    11: dayEleven,
+    12: dayTwelve,
+    13: dayThirteen,
+  };
 
-	let days: string[];
+  let days: string[];
 
-	if (process.argv.length < 3) {
-		days = Object.keys(availableDays);
-	} else {
-		days = process.argv.slice(2);
-	}
+  if (process.argv.length < 3) {
+    days = Object.keys(availableDays);
+  } else {
+    days = process.argv.slice(2);
+  }
 
-	for (const day of days) {
-		if (!availableDays[day] || !fs.existsSync(day)) {
-			console.error(`Day ${day} is not available`);
-			process.exit(1);
-		}
+  for (const day of days) {
+    if (!availableDays[day] || !fs.existsSync(day)) {
+      console.error(`Day ${day} is not available`);
+      process.exit(1);
+    }
 
-		if (
-			!numberStrings[day] ||
-			!fs.existsSync(`../shared/${numberStrings[day]}.txt`)
-		) {
-			console.error(`Day ${day} is missing it's input file`);
-			process.exit(1);
-		}
-	}
+    if (
+      !numberStrings[day] ||
+      !fs.existsSync(`../shared/${numberStrings[day]}.txt`)
+    ) {
+      console.error(`Day ${day} is missing it's input file`);
+      process.exit(1);
+    }
+  }
 
-	const results = days.map((day) => {
-		const dayString = numberStrings[day];
-		const input = fs.readFileSync(`../shared/${dayString}.txt`, "utf8");
+  const results = days.map((day) => {
+    const dayString = numberStrings[day];
+    const input = fs.readFileSync(`../shared/${dayString}.txt`, "utf8");
 
-		const startTime = Date.now();
-		const result = availableDays[day](input);
-		const endTime = Date.now();
+    const startTime = Date.now();
+    const result = availableDays[day](input);
+    const endTime = Date.now();
 
-		return {
-			day: parseInt(day, 10),
-			partOne: result[0],
-			partTwo: result[1],
-			time: endTime - startTime,
-		};
-	});
+    return {
+      day: parseInt(day, 10),
+      partOne: result[0],
+      partTwo: result[1],
+      time: endTime - startTime,
+    };
+  });
 
-	printAsTable(results);
+  printAsTable(results);
 }
 
 main();
