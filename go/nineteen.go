8e098815c637a4bd030f70f292fec7d9d139e6bf
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2024-10-04 12:15:28 +0200

    wip(go): implement part two of day nineteen in go

--- go/nineteen.go
+++ go/nineteen.go
@@ -153,8 +153,146 @@ func nineteenPartOne(input string) int {
 	return result
 }
 
+type NegatableRule struct {
+	rule    Rule
+	negated bool
+}
+
+type RuleTreePath struct {
+	rules    []NegatableRule
+	accepted bool
+}
+
+type RuleTree []RuleTreePath
+
+type CategoryRange struct {
+	min int
+	max int
+}
+
+func buildRuleTree(rule Rule, workflows Workflows) RuleTree {
+	if rule.category == "" {
+		rules := []NegatableRule{{rule, false}}
+		accepted := rule.result == Accepted
+
+		return RuleTree{{rules, accepted}}
+	}
+
+	if rule.result == Accepted || rule.result == Rejected {
+		rules := []NegatableRule{{rule, false}}
+		negatedRules := []NegatableRule{{rule, true}}
+		accepted := rule.result == Accepted
+
+		return RuleTree{{rules, accepted}, {negatedRules, !accepted}}
+	}
+
+	startRules := []NegatableRule{{rule, false}}
+	ruleTree := RuleTree{}
+
+	nextWorkflow := workflows[rule.result]
+
+	for i := 0; i < len(nextWorkflow); i++ {
+		nextRule := nextWorkflow[i]
+		previousRules := startRules
+
+		for j := 0; j < i; j++ {
+			previousRules = append(previousRules, NegatableRule{nextWorkflow[j], true})
+		}
+
+		treeFromNextRule := buildRuleTree(nextRule, workflows)
+
+		for j := 0; j < len(treeFromNextRule); j++ {
+			newRules := append(previousRules, treeFromNextRule[j].rules...)
+			accepted := treeFromNextRule[j].accepted
+			ruleTree = append(ruleTree, RuleTreePath{newRules, accepted})
+		}
+	}
+
+	return ruleTree
+}
+
 func nineteenPartTwo(input string) int {
-	return 0
+	workflows := Workflows{}
+	inputParts := strings.Split(input, "\n\n")
+	workflowLines := strings.Split(inputParts[0], "\n")
+
+	for i := 0; i < len(workflowLines); i++ {
+		line := workflowLines[i]
+
+		if len(line) == 0 {
+			continue
+		}
+
+		name, workflow := parseWorkflow(line)
+		workflows[name] = workflow
+	}
+
+	startWorkflow := workflows["in"]
+	ruleTree := RuleTree{}
+	result := 0
+
+	for i := 0; i < len(startWorkflow); i++ {
+		rule := startWorkflow[i]
+		previousRules := []NegatableRule{{rule, false}}
+
+		for j := 0; j < i; j++ {
+			previousRules = append(previousRules, NegatableRule{startWorkflow[j], true})
+		}
+
+		treeFromRule := buildRuleTree(rule, workflows)
+
+		for j := 0; j < len(treeFromRule); j++ {
+			path := treeFromRule[j]
+
+			if !path.accepted {
+				continue
+			}
+
+			newRules := append(previousRules, path.rules...)
+			ruleTree = append(ruleTree, RuleTreePath{newRules, true})
+		}
+	}
+
+	for _, path := range ruleTree {
+		ranges := map[string]CategoryRange{
+			"x": {1, 4000},
+			"m": {1, 4000},
+			"a": {1, 4000},
+			"s": {1, 4000},
+		}
+
+		// len-1 because the last rule has no operation (just result=accepted)
+		for i := 0; i < len(path.rules)-1; i++ {
+			rule := path.rules[i]
+			operation, negated, category, compareVal := rule.rule.operation, rule.negated, rule.rule.category, rule.rule.compareVal
+
+			if (operation == Gt && !negated) || (operation == Lt && negated) {
+				ranges[category] = CategoryRange{compareVal + 1, ranges[category].max}
+			}
+
+			if (operation == Lt && !negated) || (operation == Gt && negated) {
+				ranges[category] = CategoryRange{ranges[category].min, compareVal - 1}
+			}
+		}
+
+		hasInvalidRange := false
+		rangesResult := 1
+
+		for _, val := range ranges {
+			if val.min > val.max {
+				hasInvalidRange = true
+				break
+			}
+
+			rangesResult *= val.max + 1 - val.min
+		}
+
+		if !hasInvalidRange {
+			result += rangesResult
+		}
+	}
+
+	return result
 }
 
 func nineteen(input string) (int, int) {
--- go/nineteen_test.go
+++ go/nineteen_test.go
@@ -27,3 +27,47 @@ hdj{m>838:A,pv}
 		t.Fatalf("Expected %d but was %d", expected, actual)
 	}
 }
+
+func TestNineteenPartTwo(t *testing.T) {
+	input := `px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}`
+
+	expected := 167409079868000
+
+	if actual := nineteenPartTwo(input); actual != expected {
+		t.Fatalf("Expected %d but was %d", expected, actual)
+	}
+}
+
+func TestBuildRuleTree(t *testing.T) {
+	workflows := Workflows{}
+	workflows["px"] = Workflow{{"a", "<", 2006, "qkq"}, {"m", ">", 2090, "A"}, {"", "", 0, "rfg"}}
+	workflows["pv"] = Workflow{{"a", ">", 1716, "R"}, {"", "", 0, "A"}}
+	workflows["lnx"] = Workflow{{"m", ">", 1548, "A"}, {"", "", 0, "A"}}
+	workflows["rfg"] = Workflow{{"s", "<", 537, "gd"}, {"x", ">", 2440, "R"}, {"", "", 0, "A"}}
+	workflows["qs"] = Workflow{{"s", ">", 3448, "A"}, {"", "", 0, "lnx"}}
+	workflows["qkq"] = Workflow{{"x", "<", 1416, "A"}, {"", "", 0, "crn"}}
+	workflows["crn"] = Workflow{{"x", ">", 2662, "A"}, {"", "", 0, "R"}}
+	workflows["in"] = Workflow{{"s", "<", 1351, "px"}, {"", "", 0, "qqz"}}
+	workflows["qqz"] = Workflow{{"s", ">", 2770, "qs"}, {"m", "<", 1801, "hdj"}, {"", "", 0, "R"}}
+	workflows["gd"] = Workflow{{"a", ">", 3333, "R"}, {"", "", 0, "R"}}
+	workflows["hdj"] = Workflow{{"m", ">", 838, "A"}, {"", "", 0, "pv"}}
+
+	// rule1 := Rule{"m", ">", 838, "A"}
+	// expected1 := RuleTree{{{{"m", ">", 838, "A"}, false}, {{"m", ">", 838, "A"}, }}}
+}
