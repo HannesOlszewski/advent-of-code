0c40229ac9c6a5772f3db85cbb4e562f908061ae
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2024-12-07 17:33:32 +0100

    test(2024): add dedicated tests.zig file

---
+++ 2024/zig/tests.zig
@@ -0,0 +1,9 @@
+comptime {
+    _ = @import("src/one.zig");
+    _ = @import("src/two.zig");
+    _ = @import("src/three.zig");
+    _ = @import("src/four.zig");
+    _ = @import("src/five.zig");
+    _ = @import("src/six.zig");
+    _ = @import("src/seven.zig");
+}
