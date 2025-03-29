84138bbfc8a6adf63302b971dbc6bbf0a6a94dcb
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2025-03-29 12:37:20 +0100

    perf(2015): improve ts performance for day four a little bit

--- 2015/ts/src/four.test.ts
+++ 2015/ts/src/four.test.ts
@@ -2,11 +2,11 @@ import { expect, test } from "bun:test";
 import four from "./four";
 
 test("four part one", () => {
-	expect(four.partOne("abcdef")).toBe(609043);
-	expect(four.partOne("pqrstuv")).toBe(1048970);
+  expect(four.partOne("abcdef")).toBe(609043);
+  expect(four.partOne("pqrstuv")).toBe(1048970);
 });
 
 test("four part two", () => {
-	expect(four.partTwo("abcdef")).toBe(6742839);
-	expect(four.partTwo("pqrstuv")).toBe(5714438);
+  expect(four.partTwo("abcdef")).toBe(6742839);
+  expect(four.partTwo("pqrstuv")).toBe(5714438);
 });
--- 2015/ts/src/four.ts
+++ 2015/ts/src/four.ts
@@ -1,38 +1,42 @@
 function partOne(input: string): number {
-	const str = input.trim();
-	const hasher = new Bun.CryptoHasher("md5");
+  const str = input.trim();
+  const hasher = new Bun.CryptoHasher("md5");
+  const result = new Uint8Array(64);
 
-	let i = 0;
-	while (true) {
-		i++;
-		const hash = hasher.update(`${str}${i}`).digest("hex");
+  let i = 1;
+  while (true) {
+    i++;
+    hasher.update(`${str}${i}`);
+    const hash = hasher.digest(result);
 
-		if (hash.startsWith("00000")) {
-			break;
-		}
-	}
+    if (hash[0] === 0 && hash[1] === 0 && hash[2] <= 15) {
+      break;
+    }
+  }
 
-	return i;
+  return i;
 }
 
 function partTwo(input: string): number {
-	const str = input.trim();
-	const hasher = new Bun.CryptoHasher("md5");
+  const str = input.trim();
+  const hasher = new Bun.CryptoHasher("md5");
+  const result = new Uint8Array(64);
 
-	let i = 0;
-	while (true) {
-		i++;
-		const hash = hasher.update(`${str}${i}`).digest("hex");
+  let i = 1;
+  while (true) {
+    i++;
+    hasher.update(`${str}${i}`);
+    const hash = hasher.digest(result);
 
-		if (hash.startsWith("000000")) {
-			break;
-		}
-	}
+    if (hash[0] === 0 && hash[1] === 0 && hash[2] === 0) {
+      break;
+    }
+  }
 
-	return i;
+  return i;
 }
 
 export default {
-	partOne,
-	partTwo,
+  partOne,
+  partTwo,
 };
