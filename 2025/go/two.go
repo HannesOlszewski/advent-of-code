5db4722dc1cadb73ce6e2416661eb16d9cf56718
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2025-12-02 11:58:53 +0100

    feat(2025): implement day two in go

--- 2025/go/two.go
+++ 2025/go/two.go
@@ -1,9 +1,114 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
+func isInvalidIdPartOne(id string) bool {
+	if len(id)%2 == 1 {
+		return false
+	}
+
+	digitCountToCheck := len(id) / 2
+
+	for i := range digitCountToCheck {
+		if id[i] != id[i+digitCountToCheck] {
+			return false
+		}
+	}
+
+	return true
+}
+
 func DayTwoPartOne(input string) string {
-	return ""
+	var result int64 = 0
+	ranges := strings.SplitSeq(input, ",")
+
+	for idRange := range ranges {
+		ids := strings.Split(idRange, "-")
+
+		if len(ids) != 2 {
+			panic("Wrong id range format")
+		}
+
+		start, err1 := strconv.ParseInt(ids[0], 10, 64)
+		end, err2 := strconv.ParseInt(ids[1], 10, 64)
+
+		if err1 != nil || err2 != nil {
+			panic("Could not parse ids")
+		}
+
+		for id := start; id <= end; id++ {
+			idString := strconv.FormatInt(id, 10)
+			if isInvalidIdPartOne(idString) {
+				result += id
+			}
+		}
+
+	}
+
+	return strconv.FormatInt(result, 10)
+}
+
+func isInvalidIdPartTwo(id string) bool {
+	maxDigitCountToCheck := len(id) / 2
+
+	for patternLength := range maxDigitCountToCheck + 1 {
+		if patternLength == 0 || len(id)%patternLength != 0 {
+			continue
+		}
+
+		pattern := id[:patternLength]
+		isMatch := true
+
+		for compareStartIndex := patternLength; compareStartIndex < len(id); compareStartIndex += patternLength {
+			for digitIndex := range patternLength {
+				if pattern[digitIndex] != id[compareStartIndex+digitIndex] {
+					isMatch = false
+					break
+				}
+			}
+
+			if !isMatch {
+				break
+			}
+		}
+
+		if isMatch {
+			return true
+		}
+	}
+
+	return false
 }
 
 func DayTwoPartTwo(input string) string {
-	return ""
+	var result int64 = 0
+	ranges := strings.SplitSeq(input, ",")
+
+	for idRange := range ranges {
+		ids := strings.Split(idRange, "-")
+
+		if len(ids) != 2 {
+			panic("Wrong id range format")
+		}
+
+		start, err1 := strconv.ParseInt(ids[0], 10, 64)
+		end, err2 := strconv.ParseInt(ids[1], 10, 64)
+
+		if err1 != nil || err2 != nil {
+			panic("Could not parse ids")
+		}
+
+		for id := start; id <= end; id++ {
+			idString := strconv.FormatInt(id, 10)
+			if isInvalidIdPartTwo(idString) {
+				result += id
+			}
+		}
+
+	}
+
+	return strconv.FormatInt(result, 10)
 }
--- 2025/go/two_test.go
+++ 2025/go/two_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestDayTwoPartOne(t *testing.T) {
-	input := ``
+	input := "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 
-	expected := ""
+	expected := "1227775554"
 
 	if actual := DayTwoPartOne(input); actual != expected {
 		t.Fatalf("Expected %v but got %v", expected, actual)
@@ -13,9 +13,9 @@ func TestDayTwoPartOne(t *testing.T) {
 }
 
 func TestDayTwoPartTwo(t *testing.T) {
-	input := ``
+	input := "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 
-	expected := ""
+	expected := "4174379265"
 
 	if actual := DayTwoPartTwo(input); actual != expected {
 		t.Fatalf("Expected %v but got %v", expected, actual)
