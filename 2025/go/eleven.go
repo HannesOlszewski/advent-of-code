64342450c2e608f4c2faf0ce635ed223944818cf
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2025-12-13 19:54:01 +0100

    feat(2025): impelment day eleven in go

--- 2025/go/eleven.go
+++ 2025/go/eleven.go
@@ -1,9 +1,103 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func DayElevenPartOne(input string) string {
-	return ""
+	lines := strings.SplitSeq(input, "\n")
+	connections := map[string][]string{}
+	total := 0
+	const start = "you"
+	const end = "out"
+
+	for line := range lines {
+		// format is aaa: bbb ccc ...
+		//           ^^^^^^^^^^
+		//        i= 0123456789...
+		connections[line[:3]] = strings.Split(line[5:], " ")
+	}
+
+	queue := connections[start]
+
+	for len(queue) > 0 {
+		device := queue[0]
+		queue = queue[1:]
+
+		if device == end {
+			total++
+		} else {
+			queue = append(queue, connections[device]...)
+		}
+	}
+
+	return strconv.Itoa(total)
+}
+
+func findConnectionPaths(from string, to string, currentPath *[]string, validPathsCount *int, connections map[string][]string, cache map[string]bool) {
+	if _, inCache := cache[from]; inCache {
+		// already marked as not reaching the goal
+		return
+	}
+
+	if from == to {
+		*validPathsCount++
+		return
+	}
+
+	*currentPath = append(*currentPath, from)
+	nextDevices, exists := connections[from]
+
+	if !exists || len(nextDevices) == 0 {
+		cache[from] = false
+		return
+	}
+
+	for _, device := range nextDevices {
+		pathLengthBefore := len(*currentPath)
+		findConnectionPaths(device, to, currentPath, validPathsCount, connections, cache)
+
+		pathLengthAfter := len(*currentPath)
+		if pathLengthBefore < pathLengthAfter {
+			*currentPath = (*currentPath)[:pathLengthBefore]
+		}
+	}
+
+	for _, device := range nextDevices {
+		_, inCache := cache[device]
+
+		if !inCache {
+			// if it is not in the cache, it (might still) lead to the goal
+			return
+		}
+	}
+
+	// all next devices must have been in the cache (being false)
+	cache[from] = false
 }
 
 func DayElevenPartTwo(input string) string {
-	return ""
+	lines := strings.SplitSeq(input, "\n")
+	connections := map[string][]string{}
+	const start = "svr"
+	const end = "out"
+
+	for line := range lines {
+		// format is aaa: bbb ccc ...
+		//           ^^^^^^^^^^
+		//        i= 0123456789...
+		connections[line[:3]] = strings.Split(line[5:], " ")
+	}
+
+	total1 := 0
+	findConnectionPaths(start, "fft", &[]string{}, &total1, connections, map[string]bool{})
+
+	total2 := 0
+	findConnectionPaths("fft", "dac", &[]string{}, &total2, connections, map[string]bool{})
+
+	total3 := 0
+	findConnectionPaths("dac", end, &[]string{}, &total3, connections, map[string]bool{})
+
+	return strconv.Itoa(total1 * total2 * total3)
 }
--- 2025/go/eleven_test.go
+++ 2025/go/eleven_test.go
@@ -3,9 +3,18 @@ package main
 import "testing"
 
 func TestDayElevenPartOne(t *testing.T) {
-	input := ``
+	input := `aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out`
 
-	expected := ""
+	expected := "5"
 
 	if actual := DayElevenPartOne(input); actual != expected {
 		t.Fatalf("Expected %v but got %v", expected, actual)
@@ -13,9 +22,21 @@ func TestDayElevenPartOne(t *testing.T) {
 }
 
 func TestDayElevenPartTwo(t *testing.T) {
-	input := ``
+	input := `svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out`
 
-	expected := ""
+	expected := "2"
 
 	if actual := DayElevenPartTwo(input); actual != expected {
 		t.Fatalf("Expected %v but got %v", expected, actual)
